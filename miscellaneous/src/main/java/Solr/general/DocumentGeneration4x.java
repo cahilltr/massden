70a1a6dea296b2a722a5f37fb7249612dd0e2702
cahillt <cahillt@cahillts-MacBook-Pro.local>
2015-09-09 09:51:27 -0500

    cleaned up DocumentGeneration

--- miscellaneous/src/main/java/Solr/general/DocumentGeneration4x.java
+++ miscellaneous/src/main/java/Solr/general/DocumentGeneration4x.java
@@ -1,5 +1,6 @@
 package Solr.general;
 
+import org.apache.commons.cli.*;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.CloudSolrServer;
 import org.apache.solr.common.SolrInputDocument;
@@ -17,16 +18,41 @@ import java.io.IOException;
 public class DocumentGeneration4x {
   private final static Logger logger = LoggerFactory.getLogger(DocumentGeneration4x.class);
 
-  public static void main(String[] args) throws IOException, SolrServerException {
+  public static void main(String[] args) throws IOException, SolrServerException, ParseException {
+    
+    Option connectionOpt = Option.builder("SolrConnection")
+            .required(true)
+            .longOpt("SolrConnection")
+            .build();
+
+    Option collectionOpt = Option.builder("SolrCollection")
+            .required(true)
+            .longOpt("SolrCollection")
+            .build();
+
+    Option numDocsOpt = Option.builder("NumDocs")
+            .required(false)
+            .longOpt("NumDocs")
+            .build();
 
 
-    CloudSolrServer cloudSolrServer = new CloudSolrServer(args[0]);
-    cloudSolrServer.setDefaultCollection(args[1]);
+    Options options = new Options();
+    options.addOption(connectionOpt);
+    options.addOption(collectionOpt);
 
-    int totalDocs = 1000;
-    for (int i = 0; i < totalDocs; i++) {
+    CommandLineParser parser = new DefaultParser();
+
+    CommandLine cmd = parser.parse(options, args);
+    String solrConnect = cmd.getOptionValue("SolrConnection");
+    String solrCollection = cmd.getOptionValue("SolrCollection");
+    int numDocs = Integer.parseInt(cmd.getOptionValue("numDocsOpt", "1000"));
+
+    CloudSolrServer cloudSolrServer = new CloudSolrServer(solrConnect);
+    cloudSolrServer.setDefaultCollection(solrCollection);
+
+    for (int i = 0; i < numDocs; i++) {
       SolrInputDocument document = new SolrInputDocument();
-      document.addField("id", "" + i);
+      document.addField("id", "" + i + i);
       document.addField("test_i", i);
       document.addField("test_s", "this is a string");
       document.addField("test_t", "this is some text");
