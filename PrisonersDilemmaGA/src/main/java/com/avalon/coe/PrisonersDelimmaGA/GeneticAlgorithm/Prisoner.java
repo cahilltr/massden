7ab554b412c3af2c980069c2a9f418750f35ba05
cahillt <cahillt@cahillts-MacBook-Pro.local>
2015-09-12 07:51:26 -0500

    reworked code

---
+++ PrisonersDilemmaGA/src/main/java/com/avalon/coe/PrisonersDelimmaGA/GeneticAlgorithm/Prisoner.java
@@ -0,0 +1,25 @@
+package com.avalon.coe.PrisonersDelimmaGA.GeneticAlgorithm;
+
+import java.util.BitSet;
+
+/**
+ * Prisoner Class maintains prisoner strategy
+ * and returns moves based on previous history
+ */
+public class Prisoner {
+
+  public boolean playRound(int iteration, BitSet history) {
+
+    if (iteration < 3) {
+      //return strategy.get(iteration)
+    } else {
+
+    }
+
+
+    return true;
+  }
+
+
+
+}
---
+++ PrisonersDilemmaGA/src/main/java/com/avalon/coe/PrisonersDelimmaGA/prework/Chromosome.java
@@ -0,0 +1,25 @@
+package com.avalon.coe.PrisonersDelimmaGA.prework;
+
+/**
+ * Chromosome Class
+ */
+public class Chromosome {
+
+  private final int size;
+  private String chromosome;
+
+  public Chromosome(int size) {
+    this.size = size;
+  }
+
+  public void generateInitalChromosome() {
+
+    for (int i = 0 ; i < this.size; i++) {
+
+    }
+
+
+  }
+
+
+}
