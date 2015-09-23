6890bb129dbcc3eb880aab142f108a651b7fe603
cahillt <cahillt@cahillts-MacBook-Pro.local>
2015-09-23 12:01:11 -0500

    update setup.sh to change /etc/hosts

--- vagrant_cluster/setup.sh
+++ vagrant_cluster/setup.sh
@@ -18,6 +18,9 @@ sudo service ntpd start
 sudo chkconfig ntpd on
 
 sudo sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/sysconfig/selinux
+sudo sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
 sudo setenforce 0
+
+sed -i 's/^127.0.0.1.*/127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain4/' /etc/hosts
 
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
