3ced5f1896a879ca36b3bb91d121a4441b7074c1
cahilltr <trey.r.cahill@gmail.com>
2015-11-12 15:49:34 -0500

    added dateToSQLDate.py

---
+++ MiscScripts/dateToSQLDate.py
@@ -0,0 +1,19 @@
+import time
+import pandas
+
+def changeTime(value):
+    s = time.strptime(value, "%d-%b-%y")
+    # yyyy-MM-dd hh:mm:ss - Format for SQL DATE
+    d = time.strftime('%Y-%m-%d %I:%M:%S', s)
+    return d
+
+w = "5-Jan-09"
+print changeTime(w)
+
+dataFile = raw_input("Path to CSV: ")
+
+dataDF = pandas.read_csv(dataFile)
+
+dataDF['date'] = dataDF['date'].apply(changeTime)
+
+dataDF.to_csv(path_or_buf="/Users/cahillt/Downloads/StockIndexs.csv", index=False)
