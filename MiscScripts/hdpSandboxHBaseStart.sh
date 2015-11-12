e1436d9d3d74be2171194ac58af39258a26e9247
cahilltr <trey.r.cahill@gmail.com>
2015-11-12 09:44:19 -0500

    added script to start hbase and turn off other memory sucking things via Ambari's rest api'

---
+++ MiscScripts/hdpSandboxHBaseStart.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#Stop Oozie
+curl -k -u admin:admin -H "X-Requested-By:ambari" -i -X PUT -d '{"RequestInfo":{"context":"Install OOZIE_SERVER"},"Body":{"HostRoles":{"state":"INSTALLED"}}}' http://127.0.0.1:8080/api/1/clusters/Sandbox/hosts/sandbox.hortonworks.com/host_components/OOZIE_SERVER
+
+#Stop Atlas
+curl -k -u admin:admin -H "X-Requested-By:ambari" -i -X PUT -d '{"ServiceInfo":{"state":"INSTALLED"}}' http://127.0.0.1:8080/api/v1/clusters/Sandbox/services/ATLAS/
+
+#Stop Hive
+curl -k -u admin:admin -H "X-Requested-By:ambari" -i -X PUT -d '{"ServiceInfo":{"state":"INSTALLED"}}' http://127.0.0.1:8080/api/v1/clusters/Sandbox/services/HIVE/
+
+#Start HBase TODO Make Me start HBase
+curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Turn off maintenance mode for HBase"}, "Body": {"ServiceInfo": {"maintenance_state": "OFF"}}}' http://localhost:8080/api/v1/clusters/Sandbox/services/HBASE
+
+curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start HBASE via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://localhost:8080/api/v1/clusters/Sandbox/services/HBASE
