60d5fadf24879ee8a112801e717997437a205286
cahilltr <trey.r.cahill@gmail.com>
2015-11-18 19:00:17 -0500

    Reworked the index python script to use more pandas friendly techniques

--- ElasticsearchDoctors/ElasticsearchPython/IndexDoctorData.py
+++ ElasticsearchDoctors/ElasticsearchPython/IndexDoctorData.py
@@ -4,28 +4,35 @@ import pandas
 import json
 import math
 import threading
+import string
 
-removeableCols = ["Claims based hospital affiliation CCN 1","Claims based hospital affiliation LBN 1",
-                  "Claims based hospital affiliation CCN 2","Claims based hospital affiliation LBN 2",
-                  "Claims based hospital affiliation CCN 3","Claims based hospital affiliation LBN 3",
-                  "Claims based hospital affiliation CCN 4","Claims based hospital affiliation LBN 4",
-                  "Claims based hospital affiliation CCN 5","Claims based hospital affiliation LBN 5",
-                  "Professional accepts Medicare Assignment","Participating in eRx","Participating in PQRS",
-                  "Participating in EHR","Received PQRS Maintenance of Certification Program Incentive",
-                  "Participated in Million Hearts","Line 2 Street Address","Marker of address line 2 suppression",
-                  "Organization legal name","Organization DBA name","Group Practice PAC ID","Number of Group Practice members",
-                  "PAC ID","Professional Enrollment ID"]
+removeableCols = ["Claims based hospital affiliation CCN 1", "Claims based hospital affiliation LBN 1",
+                  "Claims based hospital affiliation CCN 2", "Claims based hospital affiliation LBN 2",
+                  "Claims based hospital affiliation CCN 3", "Claims based hospital affiliation LBN 3",
+                  "Claims based hospital affiliation CCN 4", "Claims based hospital affiliation LBN 4",
+                  "Claims based hospital affiliation CCN 5", "Claims based hospital affiliation LBN 5",
+                  "Professional accepts Medicare Assignment", "Participating in eRx", "Participating in PQRS",
+                  "Participating in EHR", "Received PQRS Maintenance of Certification Program Incentive",
+                  "Participated in Million Hearts", "Line 2 Street Address", "Marker of address line 2 suppression",
+                  "Organization legal name", "Organization DBA name", "Group Practice PAC ID",
+                  "Number of Group Practice members",
+                  "PAC ID", "Professional Enrollment ID"]
 
 usedCols = ['NPI', 'Last Name', 'First Name', 'Middle Name', 'Suffix', 'Gender', 'Credential', 'Medical school name',
             'Graduation year', 'Primary specialty', 'Secondary specialty 1', 'Secondary specialty 2',
             'Secondary specialty 3', 'Secondary specialty 4', 'All secondary specialties', 'Line 1 Street Address',
             'City', 'State', 'Zip Code']
 
-colsDict = {"NPI":"npi", "Last Name":"last_name", "First Name":"first_name", "Middle Name":"middle_name", "Suffix":"suffix",
-            "Gender":"gender", "Credential":"credentials", "Medical school name":"med_school", "Graduation year":"grad_year",
-            "Primary specialty":"primary_spec", "Secondary specialty 1":"secondary_spec", "Secondary specialty 2":"tertiary_spec",
-            "Secondary specialty 3":"quaternary_spec","Secondary specialty 4":"quinary_spec","All secondary specialties":"all_spec",
-            "Line 1 Street Address":"street", "City":"city","State":"state","Zip Code":"zip"}
+colsDict = {"NPI": "npi", "Last Name": "last_name", "First Name": "first_name", "Middle Name": "middle_name",
+            "Suffix": "suffix",
+            "Gender": "gender", "Credential": "credentials", "Medical school name": "med_school",
+            "Graduation year": "grad_year",
+            "Primary specialty": "primary_spec", "Secondary specialty 1": "secondary_spec",
+            "Secondary specialty 2": "tertiary_spec",
+            "Secondary specialty 3": "quaternary_spec", "Secondary specialty 4": "quinary_spec",
+            "All secondary specialties": "all_spec",
+            "Line 1 Street Address": "street", "City": "city", "State": "state", "Zip Code": "zip"}
+
 
 def createIndexAndAddMapping(es, body, indexName):
     if not es.indices.exists(index=indexName):
@@ -41,7 +48,8 @@ def createIndexAndAddMapping(es, body, indexName):
         print "index exists"
         return False
 
-def indexDataBulk(es,body):
+
+def indexDataBulk(es, body):
     if not body:
         return False
     try:
@@ -52,61 +60,54 @@ def indexDataBulk(es,body):
         return False
     return True
 
-#handle csv and return a list of DFs
+
+# handle csv and return a list of DFs
 def handleCSV(csvFile):
     dataDF = pandas.read_csv(csvFile)
-    dataDF.drop(removeableCols,axis=1,inplace=True)
+    dataDF.drop(removeableCols, axis=1, inplace=True)
     dfs = []
     step = 1000
-    for i in range(step,len(dataDF) + step, step):
-        tempDF = dataDF[i-step:i]
+    for i in range(step, len(dataDF) + step, step):
+        tempDF = dataDF[i - step:i]
         dfs.append(tempDF)
     return dfs
     # return dataDF
 
-#{ "index": { "_index": "addr", "_type": "contact", "_id": 1 }}
-#{ "name": "Fyodor Dostoevsky", "country": "RU" }
-#createJSON
-#For each row in DF, create index and index body and append to body
-def createJSONBody(df, indexName, type):
-    counted = 0
-    try:
-        body = ""
-        for index,row in df.iterrows():
-            data = {}
-            # print index, row
-            for col in usedCols:
-                if isinstance(row[col], (int, long, float, complex)):
-                    if not math.isnan(row[col]):
-                        if col == "Zip Code":
-                            zipCode = int(str(row[col])[0:5])
-                            data[colsDict[col]] = zipCode
-                        else:
-                            data[colsDict[col]] = row[col]
-                elif col == "Line 1 Street Address":
-                    street = row[col]
-                    data[colsDict[col]] = street
-                elif col == "City":
-                    city = row[col]
-                    data[colsDict[col]] = city
-                elif col == "State":
-                    state = row[col]
-                    data[colsDict[col]] = state
+
+# { "index": { "_index": "addr", "_type": "contact", "_id": 1 }}
+# { "name": "Fyodor Dostoevsky", "country": "RU" }
+# createJSON
+# create index and index body from a row
+def createJsonFromRow(row, indexName, type):
+    data = {}
+    for col in usedCols:
+        if isinstance(row[col], (int, long, float, complex)):
+            if not math.isnan(row[col]):
+                if col == "Zip Code":
+                    zipCode = int(str(row[col])[0:5])
+                    data[colsDict[col]] = zipCode
                 else:
                     data[colsDict[col]] = row[col]
-            jsonBody = json.dumps(data)
-            body += '{ "index": { "_index": "%s", "_type": "%s", "_id": %s }}' % (indexName, type, index) + "\n" + jsonBody + "\n"
-            counted += 1
-            if counted % 100 == 0:
-                print counted
-        return body
-    except:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        print (exc_type,exc_tb.tb_lineno)
+        elif col == "Line 1 Street Address":
+            street = row[col]
+            data[colsDict[col]] = street
+        elif col == "City":
+            city = row[col]
+            data[colsDict[col]] = city
+        elif col == "State":
+            state = row[col]
+            data[colsDict[col]] = state
+        else:
+            data[colsDict[col]] = row[col]
+    jsonBody = json.dumps(data)
+    return '{ "index": { "_index": "%s", "_type": "%s", "_id": %s }}' % (
+    indexName, type, row.name) + "\n" + jsonBody + "\n"
+
 
 def indexBody(es, df, indexName, type):
-    body = createJSONBody(df, indexName,type)
-    indexDataBulk(es, body)
+    df['json'] = df.apply(lambda x: createJsonFromRow(x, indexName, type), axis=1)
+    indexDataBulk(es, string.join(df['json'], "\n"))
+
 
 try:
     dataFile = raw_input("Path to CSV: ")
@@ -116,21 +117,19 @@ try:
 
     es = Elasticsearch()
 
-    with open (mapping, "r") as myfile:
-        mappingString=myfile.read()
+    with open(mapping, "r") as myfile:
+        mappingString = myfile.read()
 
     created = createIndexAndAddMapping(es, mappingString, indexName)
     if not created:
         raise
 
-    # dataDF = handleCSV(dataFile)
-    # body = createJSONBody(dataDF, indexName, typeName)
     dfList = handleCSV(dataFile)
 
     print len(dfList)
     threads = []
     for i in dfList:
-        thread = threading.Thread(target=indexBody, args=(es,i, indexName,typeName))
+        thread = threading.Thread(target=indexBody, args=(es, i, indexName, typeName))
         thread.start()
         threads.append(thread)
 
@@ -139,9 +138,6 @@ try:
     for t in threads:
         t.join()
 
-    #index JSON
-    #indexDataBulk(es, body)
-
 except:
     exc_type, exc_obj, exc_tb = sys.exc_info()
-    print (exc_type,exc_tb.tb_lineno)
+    print (exc_type, exc_tb.tb_lineno)
