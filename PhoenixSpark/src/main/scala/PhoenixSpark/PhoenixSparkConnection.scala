382d1364d6e9f6e5d736c6613e5ebca53834d158
cahilltr <trey.r.cahill@gmail.com>
2016-01-28 11:33:28 -0500

    update phoenix spark

--- PhoenixSpark/src/main/scala/PhoenixSpark/PhoenixSparkConnection.scala
+++ PhoenixSpark/src/main/scala/PhoenixSpark/PhoenixSparkConnection.scala
@@ -1,12 +1,16 @@
 package PhoenixSpark
 
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.{Column, SQLContext}
 
 /**
   * Created by cahillt on 11/25/15.
   */
 object PhoenixSparkConnection {
+
+  def avg(col:Column): Unit = {
+
+  }
 
   def main (args: Array[String]) {
     if (args.length != 2) {
@@ -23,7 +27,18 @@ object PhoenixSparkConnection {
       Map("table" -> table, "zkUrl" -> zkURL)
     )
 
-    System.out.println(df.count())
+    //select all SPY entries
+    df.filter(df("SYMBOL") === "SPY").show(20)
+
+//    df.agg(avg(df("OPEN")))OPEN
+//    df.filter(df("SYMBOL") === "SPY").agg(avg(df("OPEN")))
+    val stuff = df.filter(df("SYMBOL") === "SPY")
+//    stuff.agg(avg(df("OPEN")))
+//    stuff.agg(df("OPEN"))
+    val x = df.filter(df("SYMBOL") === "SPY").groupBy("SYMBOL").agg(df("OPEN"))
+
+    System.out.println("HELLO " + x)
+    System.out.println("HELLO2 " + df.count())
   }
 
 }
---
+++ PhoenixSpark/src/main/scala/SparkHBase/SparkHBaseConnection.scala
@@ -0,0 +1,13 @@
+package SparkHBase
+
+/**
+  * Created by cahillt on 12/2/15.
+  */
+object SparkHBaseConnection {
+
+  def main (args: Array[String]): Unit = {
+
+
+
+  }
+}
