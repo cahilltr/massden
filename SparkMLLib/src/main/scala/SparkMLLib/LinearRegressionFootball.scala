4a92cdb234a980d176a5618f8e681bb6931c249a
cahilltr <trey.r.cahill@gmail.com>
2016-02-24 12:30:26 -0500

    updated and added

--- SparkMLLib/src/main/scala/SparkMLLib/LinearRegressionFootball.scala
+++ SparkMLLib/src/main/scala/SparkMLLib/LinearRegressionFootball.scala
@@ -1,26 +1,24 @@
 package SparkMLLib
 
+
 import java.io.FileInputStream
 import java.util.Properties
 
 import org.apache.log4j.LogManager
-import org.apache.spark.{SparkContext, SparkConf}
-import org.apache.spark.mllib.tree.DecisionTree
-import org.apache.spark.sql.functions._
-import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.linalg.Vectors
-
+import org.apache.spark.mllib.regression.{LinearRegressionWithSGD, LabeledPoint}
+import org.apache.spark.sql.functions._
+import org.apache.spark.{SparkContext, SparkConf}
 
 /**
-  * Created by cahillt on 2/22/16.
-  * Creates decision tree to determine if a player will be a long term player in the NFL or not based on
+  * Created by cahillt on 2/24/16.
+  * Creates Linear Regression Model to determine if a player will be a long term player in the NFL or not based on
   * Combine Results and the College attended.
   */
-object DecisionTreeFootball {
+object LinearRegressionFootball {
+  val log = LogManager.getLogger(this.getClass)
 
   def main (args: Array[String]) {
-    val log = LogManager.getLogger(this.getClass)
-
     val properties = new Properties()
 
     if (args.length == 1) {
@@ -31,30 +29,14 @@ object DecisionTreeFootball {
     //Long Term Definition
     val longTerm = properties.getProperty("long.term", "5").toInt
     log.info("Long Term: " + longTerm)
-    //Impurity to use
-    val impurityString = properties.getProperty("impurity", "entropy")
-    log.info("Impurity: " + impurityString)
-    //Max Depth
-    val max_depth = properties.getProperty("max.depth", "10").toInt
-    log.info("Max Depth: " + max_depth)
-    //Max Bins addition to add to collegeMap.size
-    val max_bins_addition = properties.getProperty("max.bins.addition", "100").toInt
-    log.info("Max Bins Addition: " + max_bins_addition)
+    //Number of Iterations
+    val num_iterations = properties.getProperty("number.iterations", "5").toInt
+    log.info("Number of Iterations: " + num_iterations)
     //Display All output of test Data label and predictions
-    val display_label_predictions = properties.getProperty("display.label.predictions", "false").toBoolean
+    val display_label_predictions = properties.getProperty("display.label.predictions", "true").toBoolean
     log.info("Display Label Predictions: " + display_label_predictions)
-    //Display Tree Model
-    val display_tree_model = properties.getProperty("display.tree.model", "false").toBoolean
-    log.info("Display Tree Model: " + display_tree_model)
-    //Percent data that is training data
-    val training_data_percent = properties.getProperty("percent.training.data", "0.85").toDouble
-    log.info("Percent Data that is Training Data: " + training_data_percent)
-    //Percent data that is test data
-    val test_data_percent = properties.getProperty("percent.test.data", "0.15").toDouble
-    log.info("Percent Data that is Training Data: " + test_data_percent)
 
-
-    val conf = new SparkConf().setAppName("Decision_Tree").setMaster("local[*]")
+    val conf = new SparkConf().setAppName("LinearRegression").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
@@ -95,12 +77,6 @@ object DecisionTreeFootball {
       .map(t => t._1 -> t._2)
       .toMap
 
-    val numClasses = 2
-    val categoricalFeaturesInfo = Map[Int, Int](0 -> collegeMap.size)
-    val impurity = impurityString
-    val maxDepth = max_depth
-    val maxBins = collegeMap.size + max_bins_addition
-
 
     val labeledData = joinedCleaned.rdd.map(r => {
       val values = Array(collegeMap.get(r.getAs[String]("college")).get.toDouble, r.getAs[Double]("heightinchestotal"),
@@ -112,33 +88,25 @@ object DecisionTreeFootball {
       LabeledPoint(label, Vectors.dense(values))
     })
 
-    val splits = labeledData.randomSplit(Array(training_data_percent, test_data_percent))
+    val splits = labeledData.randomSplit(Array(0.85, 0.15))
     val (trainingData, testData) = (splits(0), splits(1))
-    trainingData.cache()
-    testData.cache()
 
-    val model = DecisionTree.trainClassifier(labeledData, numClasses, categoricalFeaturesInfo,
-      impurity, maxDepth, maxBins)
+    val numIterations = num_iterations
+    val model = LinearRegressionWithSGD.train(trainingData, numIterations)
 
-    trainingData.unpersist()
-
-    // Evaluate model on test instances and compute test error
-    val labelAndPreds = testData.map { point =>
+    val valuesAndPreds = testData.map { point =>
       val prediction = model.predict(point.features)
       (point.label, prediction)
     }
 
-    testData.unpersist()
-
-    if (display_tree_model) {
-      println("Learned classification tree model:\n" + model.toDebugString)
-    }
-    val testErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / testData.count()
-    println("Test Error = " + testErr)
+    val MSE = valuesAndPreds.map{case(v, p) => math.pow((v - p), 2)}.mean()
+    println("training Mean Squared Error = " + MSE)
     if (display_label_predictions) {
-      println("Predictions Count: " + labelAndPreds.count())
+      println("Predictions Count: " + valuesAndPreds.count())
       println("Actual : Prediction")
-      labelAndPreds.collect().foreach(f => println(f._1 + " : " + f._2))
+      valuesAndPreds.collect().foreach(f => println(f._1 + " : " + f._2))
     }
+
   }
+
 }
--- SparkMLLib/src/main/scala/SparkMLLib/LinearSVMFootball.scala
+++ SparkMLLib/src/main/scala/SparkMLLib/LinearSVMFootball.scala
@@ -4,23 +4,22 @@ import java.io.FileInputStream
 import java.util.Properties
 
 import org.apache.log4j.LogManager
-import org.apache.spark.{SparkContext, SparkConf}
-import org.apache.spark.mllib.tree.DecisionTree
-import org.apache.spark.sql.functions._
-import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.classification.SVMWithSGD
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
 import org.apache.spark.mllib.linalg.Vectors
-
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.sql.functions._
+import org.apache.spark.{SparkContext, SparkConf}
 
 /**
-  * Created by cahillt on 2/22/16.
-  * Creates decision tree to determine if a player will be a long term player in the NFL or not based on
+  * Created by cahillt on 2/24/16.
+  * Creates SVM Model to determine if a player will be a long term player in the NFL or not based on
   * Combine Results and the College attended.
   */
-object DecisionTreeFootball {
+object LinearSVMFootball {
+  val log = LogManager.getLogger(this.getClass)
 
   def main (args: Array[String]) {
-    val log = LogManager.getLogger(this.getClass)
-
     val properties = new Properties()
 
     if (args.length == 1) {
@@ -32,20 +31,11 @@ object DecisionTreeFootball {
     val longTerm = properties.getProperty("long.term", "5").toInt
     log.info("Long Term: " + longTerm)
     //Impurity to use
-    val impurityString = properties.getProperty("impurity", "entropy")
-    log.info("Impurity: " + impurityString)
-    //Max Depth
-    val max_depth = properties.getProperty("max.depth", "10").toInt
-    log.info("Max Depth: " + max_depth)
-    //Max Bins addition to add to collegeMap.size
-    val max_bins_addition = properties.getProperty("max.bins.addition", "100").toInt
-    log.info("Max Bins Addition: " + max_bins_addition)
+    val num_iterations = properties.getProperty("number.iterations", "100").toInt
+    log.info("Number of Iterations: " + num_iterations)
     //Display All output of test Data label and predictions
-    val display_label_predictions = properties.getProperty("display.label.predictions", "false").toBoolean
+    val display_label_predictions = properties.getProperty("display.label.predictions", "true").toBoolean
     log.info("Display Label Predictions: " + display_label_predictions)
-    //Display Tree Model
-    val display_tree_model = properties.getProperty("display.tree.model", "false").toBoolean
-    log.info("Display Tree Model: " + display_tree_model)
     //Percent data that is training data
     val training_data_percent = properties.getProperty("percent.training.data", "0.85").toDouble
     log.info("Percent Data that is Training Data: " + training_data_percent)
@@ -53,8 +43,7 @@ object DecisionTreeFootball {
     val test_data_percent = properties.getProperty("percent.test.data", "0.15").toDouble
     log.info("Percent Data that is Training Data: " + test_data_percent)
 
-
-    val conf = new SparkConf().setAppName("Decision_Tree").setMaster("local[*]")
+    val conf = new SparkConf().setAppName("SVM").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
@@ -95,13 +84,6 @@ object DecisionTreeFootball {
       .map(t => t._1 -> t._2)
       .toMap
 
-    val numClasses = 2
-    val categoricalFeaturesInfo = Map[Int, Int](0 -> collegeMap.size)
-    val impurity = impurityString
-    val maxDepth = max_depth
-    val maxBins = collegeMap.size + max_bins_addition
-
-
     val labeledData = joinedCleaned.rdd.map(r => {
       val values = Array(collegeMap.get(r.getAs[String]("college")).get.toDouble, r.getAs[Double]("heightinchestotal"),
         r.getAs[Double]("hands"), r.getAs[Double]("fortyyd"), r.getAs[Double]("twentyyd"), r.getAs[Double]("tenyd"),
@@ -114,31 +96,30 @@ object DecisionTreeFootball {
 
     val splits = labeledData.randomSplit(Array(training_data_percent, test_data_percent))
     val (trainingData, testData) = (splits(0), splits(1))
-    trainingData.cache()
-    testData.cache()
 
-    val model = DecisionTree.trainClassifier(labeledData, numClasses, categoricalFeaturesInfo,
-      impurity, maxDepth, maxBins)
+    val numIterations = num_iterations
+    val model = SVMWithSGD.train(trainingData, numIterations)
 
-    trainingData.unpersist()
+    // Clear the default threshold.
+    model.clearThreshold()
 
-    // Evaluate model on test instances and compute test error
-    val labelAndPreds = testData.map { point =>
-      val prediction = model.predict(point.features)
-      (point.label, prediction)
+    // Compute raw scores on the test set.
+    val scoreAndLabels = testData.map { point =>
+      val score = model.predict(point.features)
+      (score, point.label)
     }
 
-    testData.unpersist()
+    // Get evaluation metrics.
+    val metrics = new BinaryClassificationMetrics(scoreAndLabels)
+    val auROC = metrics.areaUnderROC()
 
-    if (display_tree_model) {
-      println("Learned classification tree model:\n" + model.toDebugString)
-    }
-    val testErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / testData.count()
-    println("Test Error = " + testErr)
+    println("Area under ROC = " + auROC)
     if (display_label_predictions) {
-      println("Predictions Count: " + labelAndPreds.count())
-      println("Actual : Prediction")
-      labelAndPreds.collect().foreach(f => println(f._1 + " : " + f._2))
+      println("Predictions Count: " + scoreAndLabels.count())
+      println("Prediction : Actual")
+      scoreAndLabels.collect().foreach(f => println(f._1 + " : " + f._2))
     }
+
   }
+
 }
