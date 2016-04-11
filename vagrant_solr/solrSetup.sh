f8e55ee92c0bacd23781ea577924c05139973da7
cahilltr <trey.r.cahill@gmail.com>
2016-04-11 08:40:32 -0400

    updated to solr 6.0

--- vagrant_solr/solrSetup.sh
+++ vagrant_solr/solrSetup.sh
@@ -23,34 +23,32 @@ sudo setenforce 0
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 
 # Setup Solr
-if [ -f /vagrant/solr-5.0.0.tgz ]; then
-	cp /vagrant/solr-5.0.0.tgz /opt/
+if [ -f /vagrant/solr-6.0.0.tgz ]; then
+	cp /vagrant/solr-6.0.0.tgz /opt/
 else
-	wget http://apache.spinellicreations.com/lucene/solr/5.0.0/solr-5.0.0.tgz
-	sudo mv solr-5.0.0.tgz /opt/
+	wget solr-6.0.0.tgz
+	sudo mv solr-6.0.0.tgz /opt/
 fi
 cd /opt/
-sudo tar zxvf solr-5.0.0.tgz
-#sudo tar zxvf solr-4.10.3.tgz
-mv /opt/solr-5.0.0 /opt/solr
-#mv /opt/solr-4.10.3 /opt/solr
+sudo tar zxvf solr-6.0.0.tgz
+mv /opt/solr-6.0.0 /opt/solr
 
 cd /opt/solr/
 
-cp /vagrant/solr-in-action.jar server/resources/
+#cp /vagrant/solr-in-action.jar server/resources/
 
-cp -r /vagrant/ch15/jts/WEB-INF/ server/webapps/
+@cp -r /vagrant/ch15/jts/WEB-INF/ server/webapps/
 cd server/webapps/
-jar -uf solr.war WEB-INF/lib/jts.jar
-cd /opt/solr/
-rm -r server/webapps/WEB-INF/
+#jar -uf solr.war WEB-INF/lib/jts.jar
+#cd /opt/solr/
+#rm -r server/webapps/WEB-INF/
 
 #move sia jar to dist
 cp /vagrant/solr-in-action.jar /opt/solr/dist/solr-in-action.jar
 
-bin/solr start -c
+#bin/solr start -c
 
-sleep 10
+#sleep 10
 
 sudo cp /vagrant/load_* /home/vagrant/
 sudo cp /vagrant/start_solr.sh /home/vagrant/
