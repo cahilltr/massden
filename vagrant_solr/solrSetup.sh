b76ddb0951ec6e9374572e8a902c8971d992a71a
cahilltr <trey.r.cahill@gmail.com>
2016-07-04 12:12:46 -0400

    updated solr to use newest and have streaming/sql examples

--- vagrant_solr/solrSetup.sh
+++ vagrant_solr/solrSetup.sh
@@ -10,7 +10,7 @@ sudo echo never > /sys/kernel/mm/redhat_transparent_hugepage/enabled
 sudo echo "echo never > /sys/kernel/mm/redhat_transparent_hugepage/defrag" >> /etc/rc.local
 sudo echo "echo never > /sys/kernel/mm/redhat_transparent_hugepage/enabled" >> /etc/rc.local
 
-sudo yum -y install wget ntp java-1.7.0-openjdk java-1.7.0-openjdk-devel
+sudo yum -y install wget ntp java-1.8.0-openjdk java-1.8.0-openjdk-devel lsof
 
 sudo service iptables stop
 sudo chkconfig iptables off
@@ -23,22 +23,22 @@ sudo setenforce 0
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 
 # Setup Solr
-if [ -f /vagrant/solr-6.0.0.tgz ]; then
-	cp /vagrant/solr-6.0.0.tgz /opt/
+if [ -f /vagrant/solr-6.1.0.tgz ]; then
+	cp /vagrant/solr-6.1.0.tgz /opt/
 else
-	wget solr-6.0.0.tgz
-	sudo mv solr-6.0.0.tgz /opt/
+	wget http://apache.mirrors.tds.net/lucene/solr/6.1.0/solr-6.1.0.tgz
+	sudo mv solr-6.1.0.tgz /opt/
 fi
 cd /opt/
-sudo tar zxvf solr-6.0.0.tgz
-mv /opt/solr-6.0.0 /opt/solr
+sudo tar zxvf solr-6.1.0.tgz
+mv /opt/solr-6.1.0 /opt/solr
 
 cd /opt/solr/
 
 #cp /vagrant/solr-in-action.jar server/resources/
 
-@cp -r /vagrant/ch15/jts/WEB-INF/ server/webapps/
-cd server/webapps/
+#cp -r /vagrant/ch15/jts/WEB-INF/ server/webapps/
+#cd server/webapps/
 #jar -uf solr.war WEB-INF/lib/jts.jar
 #cd /opt/solr/
 #rm -r server/webapps/WEB-INF/
@@ -48,9 +48,10 @@ cp /vagrant/solr-in-action.jar /opt/solr/dist/solr-in-action.jar
 
 #bin/solr start -c
 
-#sleep 10
+sleep 10
 
 sudo cp /vagrant/load_* /home/vagrant/
 sudo cp /vagrant/start_solr.sh /home/vagrant/
+chmod 557 /home/vagrant/start_solr.sh
 chown -R vagrant /home/vagrant/
 echo "setup complete"
--- vagrant_solr/start_solr.sh
+++ vagrant_solr/start_solr.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-sudo sh /opt/solr/bin/solr start -c
+#Sample Stream, Streams cant be Curled'
+#search(TestCollection, q="inStock:true", rows="100", fl="id,*", sort="id desc")
+
+#Start Solr
+sudo /opt/solr/bin/solr start -c
+
+#Create Solr Collection and add configuration to ZK
+sudo /opt/solr/bin/solr create -c TestCollection -d /opt/solr/server/solr/configsets/sample_techproducts_configs/conf/
+
+#Index All example docs
+sudo /opt/solr/bin/post -c TestCollection /opt/solr/example/exampledocs/*
+
+#Preview SQL
+curl --data-urlencode 'stmt=SELECT manu as mfr, price as retail FROM TestCollection LIMIT 20' http://localhost:8983/solr/TestCollection/sql?aggregationMode=facet
+
+#Streaming
+curl --data-urlencode 'expr=search(TestCollection, q="inStock:true", rows="100", fl="id,*", sort="id desc")' -XPOST http://localhost:8983/solr/TestCollection/stream
